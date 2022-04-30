f37da3a208c648a8a3c62ce5ffab51b4d3bb616f
robert-brown96 <robert.brown@saascg.com>
2022-04-30 13:25:58 -0500

    update working

---
+++ src/routes/NetsuiteAccounts/update.js
@@ -0,0 +1,37 @@
+import AWS from "aws-sdk";
+import * as uuid from "uuid";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event) {
+    const data = JSON.parse(event.body);
+    console.log(JSON.stringify(event));
+    const keys = Object.keys(data);
+
+    let setVals = keys.map(k => {
+        if (data[k] && data[k] !== "" && data[k] !== "null")
+            return `${k} = :${k}`;
+    });
+    console.log(`sets ${JSON.stringify(setVals)}`);
+
+    const updateParams = {
+        TableName: process.env.NS_ACCOUNT_TABLE,
+        Key: {
+            realm: event.pathParameters.id
+        },
+        UpdateExpression: `SET ${setVals.join(",")}`,
+        ExpressionAttributeValues: {
+            ...(data.tokenId && { ":tokenId": data.tokenId }),
+            ...(data.tokenSecret && { ":tokenSecret": data.tokenSecret }),
+            ...(data.consumerSecret && {
+                ":consumerSecret": data.consumerSecret
+            }),
+            ...(data.consumerKey && { ":consumerKey": data.consumerKey }),
+            ...(data.url && { ":url": data.url })
+        },
+        ReturnValues: "ALL_NEW"
+    };
+
+    // TODO: Not rejecting dups
+    return await dynamoDb.update(updateParams).promise();
+}
